ee5ba6cb26ce0a66f0709aeed7224d225a72d583
89288-Abhimanyu <abhimansing999@gmail.com>
2025-09-02 22:30:35 +0530

    feature-backend: feature/backend-User Controllers

---
+++ backend-pizza-hut/src/main/java/com/pizzahut/service/PasswordResetTokenService.java
@@ -0,0 +1,11 @@
+package com.pizzahut.service;
+
+import com.pizzahut.model.PasswordResetToken;
+
+public interface PasswordResetTokenService {
+
+	public PasswordResetToken findByToken(String token);
+
+	public void delete(PasswordResetToken resetToken);
+
+}
